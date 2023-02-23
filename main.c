9dba695afbc1c09d28c1d5636b476ef3984bd11e
Jettyjaaaaa <wish.sem@student.mahidol.edu>
2023-02-23 21:29:16 +0700

    test1

--- main.c
+++ main.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include "node.h"
 
+#include "node.h"
+#include "stack.h"
 int main(int argc, char **argv){
   
-  int i,N,j;
-  NodePtr *top;
-  
-  push(&top,5);
-  pop(&top);
-  push(&top,5);
-  push(&top,5);
- 
+  Stack s;
+  s.top = NULL;
+
+  printf("Checking the  parentheses in argv agruments");
+  for(int i =1 ; i < argc ; i++){
+    pop_all(&s);
+    printf("\nargv %d %s", i, isBalance(&s, argv[i]));
+  }
  
   
 
@@ -23,13 +24,6 @@ int main(int argc, char **argv){
    
      for(j=0;j<strlen(argv[i]);j++){
        /* Use stack to help with the parentheses*/
-
-
-     }
-
-
-  }
-*/
 
 
 
